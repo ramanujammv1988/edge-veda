e4f030c299bce4707a86d8cc43b2ee44aad540d9
Ram <ramanujammv88@gmail.com>
2026-02-22 00:54:36 +0000

    refactor(quick-10): podspec vendored_frameworks + DynamicLibrary.open + ffi ^2.0.0
    - Podspec: vendored_frameworks replaces force_load + 88-line exported_symbol whitelist
    - Podspec: remove static_framework, preserve_paths, user_target_xcconfig entirely
    - Podspec: frameworks reduced to AVFoundation, Photos, EventKit (Metal/MPS/Accelerate in dylib)
    - FFI: DynamicLibrary.open('EdgeVedaCore.framework/EdgeVedaCore') replaces DynamicLibrary.process()
    - pubspec: ffi constraint widened from >=2.0.0 <2.1.0 to ^2.0.0
    - pubspec: version bumped to 2.4.0

--- flutter/lib/src/ffi/bindings.dart
+++ flutter/lib/src/ffi/bindings.dart
@@ -951,8 +951,8 @@ class EdgeVedaNativeBindings {
     if (Platform.isAndroid) {
       return DynamicLibrary.open('libedge_veda.so');
     } else if (Platform.isIOS) {
-      // On iOS, the library is statically linked
-      return DynamicLibrary.process();
+      // On iOS, load the vendored dynamic framework
+      return DynamicLibrary.open('EdgeVedaCore.framework/EdgeVedaCore');
     } else if (Platform.isMacOS) {
       return DynamicLibrary.open('libedge_veda.dylib');
     } else if (Platform.isLinux) {
